21460e8a8f5dd85e36d2d1747a6ea0073fe72aab
valochan9850-hub <valochan9850@gmail.com>
2025-10-13 18:56:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../.../components/ui/card";
+} from "../../components/ui/card";
 import { Input } from "../../components/ui/input";
 import { useState, useEffect } from "react";
 import {
